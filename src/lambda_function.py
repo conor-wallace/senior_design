4d6e63281a53a79d7d063adeff469f2ec85d09d5
Rdecker667 <deck66777@gmail.com>
2019-03-15 13:33:47 -0500

    updated lambda function

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,40 +1,20 @@
 import json
+from flask import Flask
+from flask_ask import Ask, statement, request, session, question
 
-def lambda_handler(event, context):
-    return get_hello_message()
+app = Flask(__name__)
+ask = Ask(app, '/')
 
-def get_hello_message():
-    pubObject = object #will need slot from skill
-    pub
-    session_attributes = {}
-    card_title = "Helping Hand Hello"
-    speech_output = "Hello, my name is Helping Hand."
-    should_end_session = False
-    return build_response(session_attributes, build_speechlet_response(card_title, speech_output, None, should_end_session))
+@ask.intent('hello')
+def hello():
+    return statement("Hello User!")
 
-def build_speechlet_response(title, output, reprompt_text, should_end_session):
-    return {
-        "outputSpeech": {
-            "type": "PlainText",
-            "text": output
-        },
-        "card": {
-            "type": "Simple",
-            "title": title,
-            "content": output
-        },
-        "reprompt": {
-            "outputSpeech": {
-                "type": "PlainText",
-                "text": reprompt_text
-            }
-        },
-        "shouldEndSession": should_end_session
-    }
+@ask.intent('fetch')
+def fetchObject(object):
+    if object == 'cup' :
+        return statement('Retrieving the object')
+    else:
+        return statement('I do not know what that object is')
 
-def build_response(session_attributes, speechlet_response):
-    return {
-        "version": "1.0",
-        "sessionAttributes": session_attributes,
-        "response": speechlet_response
-}
+if __name__ == '__main__':
+    app.run(debug = True)
